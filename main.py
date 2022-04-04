78809532a576701a5f4e335a224fb57c0bc79618
Steven A Moore <stevenallenmoore@gmail.com>
2022-04-04 10:21:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+import random
+import time
 
 ########### Your Code here ##############
 # You should only have functions here
@@ -13,21 +15,37 @@ import turtle
 
 
 
-##########  Do Not Alter Any Code Past Here ########
+#########################################################
+#         Do not alter any code below here              #
+#       Your code must work with the main proivided     #
+#########################################################
 def main():
-    #Part A
-    wn = turtle.Screen()
-    wn.tracer(5)
-    dart = turtle.Turtle()
-    dart.speed(0)
-    drawSineCurve(myturtle=dart)
+    # Get number of darts for simulation from user
+    # Note continuation character <\> so we don't go over 78 columns:
+    print("This is a program that simulates throwing darts at a dartboard\n" \
+        "in order to approximate pi: The ratio of darts in a unit circle\n"\
+        "to the total number of darts in a 2X2 square should be\n"\
+        "approximately  equal to pi/4")
+    print("=========== Part A ===========")
 
-    #Part B
-    setupWindow(mywindow=wn)
-    setupAxis(myturtle=dart)
-    dart.speed(0)
-    drawSineCurve(myturtle=dart)
-    drawCosineCurve(myturtle=dart)
-    drawTangentCurve(myturtle=dart)
-    wn.exitonclick()
+    #Create window, turtle, set up window as dartboard
+    window = turtle.Screen()
+    darty = turtle.Turtle()
+    darty.speed(0) # as fast as it will go!
+    setUpDartboard(myscreen=window, myturtle=darty)
+
+    # Loop for 10 darts to test your code
+    for i in range(10):
+        throwDart(darty)
+        if isInCircle(myturtle=darty, radius=1):
+          print("You hit the dartboard!")
+    print("\tPart A Complete...")
+    print("=========== Part B ===========")
+    darty.clear()
+    setUpDartboard(myscreen=window, myturtle=darty)
+    playDarts(myturtle=darty)
+    print("\tPart B Complete...")
+    # Keep the window up until dismissed
+    window.exitonclick()
+
 main()
